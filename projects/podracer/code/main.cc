d6d08281fdab45e69b0ec09c2747e3ccae909904
Alexander <alex2.aslund@gmail.com>
2023-05-11 16:14:47 +0200

    Podracer folder added

---
+++ projects/podracer/code/main.cc
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// main.cc
+// (C) 2015-2018 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "config.h"
+#include "podracerapp.h"
+#include <iostream>
+
+int
+main(int argc, const char** argv)
+{
+    std::cout << "Hello World";
+	
+}
